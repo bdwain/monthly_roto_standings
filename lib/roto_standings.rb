f0325acd58175a7cde75e462c439c03cce8b7402
Bryan Wain <bryandwain@gmail.com>
2014-05-06 01:38:33 -0500

    hide stats from future months that haven't begun yet

--- lib/roto_standings.rb
+++ lib/roto_standings.rb
@@ -1,5 +1,6 @@
 require_relative 'roto_standings_period'
 require_relative 'team_stats'
+require 'date'
 
 class RotoStandings
     attr_reader :standings_by_period
@@ -8,6 +9,7 @@ class RotoStandings
 
     def initialize(all_team_stats)
         periods = all_team_stats[0].get_periods
+        periods.reject!{|period| period != "totals" && Date::MONTHNAMES.index(period.capitalize) > Date.today.month}
         @teams = Hash[all_team_stats.collect { |team_stats| [team_stats.team_id, team_stats.team_name] }]
         @stat_names = TeamStats::get_stat_names
         @standings_by_period = {}
